1cac8d5d432f72e59213764e98353df1cb169d0a
TheinMinHtethhh <167944582+TheinMinHtethhh@users.noreply.github.com>
2024-10-11 10:51:22 +0900

    Add files via upload
    新しいだよ迷わないでね！！！

--- InstanceTest.java
+++ InstanceTest.java
@@ -5,14 +5,24 @@ public class InstanceTest{
     public  static void main( String[]  ags ) { 
 	Scanner sc = new Scanner(System.in);
 	Convert1 cvt = new Convert1();
-       	int a = cvt.inchTocm(3.5);
-	System .out. println( "3.5インチは約 "+a+"cm"  ) ;
 
-	double b = cvt.poundTokg(9);
-	System.out.println("9ポンドは約　"+b+"kg");
+	System.out.println("あんたの身長をインチで教えてください。");
+	int userHeightInInches = sc.nextInt();
+	int heightToCm = cvt.inchTocm(userHeightInInches);
+	System .out. println( userHeightInInches +"インチ　は約"+heightToCm+"cm" +" です" ) ;
 
-	int c = cvt.FtoC(100);
-	System.out.println("100°F は　"+ c + "°C");
+	System.out.println("変換したいポンドを教えてください。");
+	double userWeightInKg= sc.nextDouble();
+	double userPoundToKg = cvt.poundTokg(userWeightInKg);
+	System.out.println(userWeightInKg +"ポンド　は約　"+userPoundToKg+"kg");
+
+	System.out.println("変換したい華氏温度を教えてください。");
+	int userTinF = sc.nextInt();
+	int FerToCel = cvt.FtoC(userTinF);
+	System.out.println( userTinF +" 華氏温度は約　"+ FerToCel + "°C"+ "です");
+
+	
+	System.out.println("僕の身長は　"+ Convert1.height*1.2 + " です");
     }
 }
 class Convert1{
@@ -20,11 +30,12 @@ class Convert1{
 	return(int)(2.54*a);
 } 
   
-    double poundTokg(int w){
+    double poundTokg(double w){
 	return (0.454*w);
 }
    int FtoC(int d){
 	return(int)((5.0/9.0)*(d-32));
 }
+    public static double height = 167.0;
 }
 
